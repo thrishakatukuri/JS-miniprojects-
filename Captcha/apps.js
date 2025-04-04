c8703ef741aa5f59e9e902eeb8b1bdf92bc7d27f
thrishakatukuri <katukurithrisha12@gmail.com>
2025-04-04 18:04:47 +0530

    Add Captcha project and quotes.html

---
+++ Captcha/apps.js
@@ -0,0 +1,26 @@
+let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let age = "";
+    for( let i=0; i< 5; i++){
+        let randomIndex = Math.floor(Math.random() * char.length);
+        age += char[randomIndex];
+   document.getElementById("captcha").textContent = age;
+
+    }
+    let count =0;
+document.getElementById('btn').addEventListener('click', (e) => {
+let age = "";
+    for( let i=0; i< 5; i++){
+        let randomIndex = Math.floor(Math.random() * char.length);
+        age += char[randomIndex];
+   document.getElementById("captcha").textContent = age;
+
+   count++;
+
+   if(count>=10){ //{(count=1)===(5counts) coz, in count 1, 5times it calls the char indexes}.
+    let btn = document.getElementById('btn')
+    btn.disabled = true;
+    btn.innerText = "disabled";
+   }
+
+    }
+});
