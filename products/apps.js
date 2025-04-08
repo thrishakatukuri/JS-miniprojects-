5ffc7969aae1cf3f6a2b360dab0fd7e2fd5a9d27
thrishakatukuri <katukurithrisha12@gmail.com>
2025-04-08 21:08:19 +0530

    sixth commit

--- products/apps.js
+++ products/apps.js
@@ -1,30 +1,40 @@
-let productsData = fetch('https://www.dummyjson.com/products')
+// let h1 = document.createElement('h1')
+// h1.innerHTML = 'Products';
+// h1.style.display='flex';
+// h1.style.flexDirection='column';
+// h1.style.width='100%';
+// document.body.appendChild(h1);
 
-// async function data() {
-//     let productsApi = await productsData;
-//     // console.log(productsApi);
-//     let products = await productsApi.json();
-//     console.log(products.products[0]);
-// }data()
-//simple of av=bove lines
+// let section = document.createElement('section');
+// section.id = 'products-container';
+// section.innerHTML = `<h1>Products</div>`
+// document.body.appendChild(section);
+
+let productsData = fetch('https://www.dummyjson.com/products')
 
 async function data() {
     let productsApi = await productsData;
-    console.log(productsApi);
 
     let {products} = await productsApi.json();
+
+    products.splice(8, 1); // Remove 9th item
+    products.splice(5, 1); // Remove 6th item
+    products.splice(16, 1); // Remove 16th item
+    // <h1>Products</h1>
+
     console.log(products);
     products.map((item) =>{
         document.body.innerHTML += `<div id=products>
-        <h1>${item.title}</h1> 
-        <img src=${item.thumbnail} width:10px; height:10px;>
-        <p>Price: ${item.price}</p>
-        <p>Rting: ${item.rating}</p>
-        <p>Reviews: ${item.reviews[0].rating}</p>
-        <p>${item.description}</p>
-        <p>stock: ${item.stock}</p>
-
+        <h1><span>${item.title}</span></h1> 
+        <img src=${item.thumbnail} >
+        <h2>Price: £  ${item.price}/-</h2>
+        <div id="review">
+        <h3>In-Stock: (${item.stock})</h3>
+        <h3>Ratings: (${item.rating})</h3>
+        </div>
+        <h4>Top-Reviews : ${item.reviews[0].comment} ...</h4>
+        <p>${item.description}..</p>
        </div> `
+
     })
 }data()
-console.log(productsData);
